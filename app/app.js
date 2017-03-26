65ff965a6149056f72e054bc6148401dcea4c5c1
Arkadi <mrarkadi@gmail.com>
2017-03-26 03:33:16 +0300

    finished up until components

--- app/app.js
+++ app/app.js
@@ -1,1 +1,34 @@
-var app = angular.module('movieLibrary', [])
+var app = angular.module('movieLibrary', []);
+
+app.directive("movieDirective", function() {
+  return {
+    templateUrl: 'app/movies/movie.html',
+    scope: {
+      movie: '=myMovies',
+      delete: '&'
+    },
+    link: function(scope, elem, attrs) {
+      scope.clickingFunc = function() {
+        //  alert('clicked!')
+        console.log("clicked!");
+        console.log(elem[0]);
+        //console.log(attrs);
+        // console.log("i is: " + i);
+        scope.delete();
+      };
+      // NOTE: This type of binding is not an angular one.
+      // all of these "onclick" and "bind"
+      // elem.bind('click', scope.clickingFunc);
+      //   elem[0].innerHTML = "Just write this!";
+      // }
+    }
+  }
+});
+
+// app.component('movieComponent', {
+//   templateUrl: 'app/movies/movie.html',
+//   controller: 'movieCtrl',
+//   bindings: {
+//     movie: '=myMovies'
+//   }
+// });
--- app/movies/moviesCtrl.js
+++ app/movies/moviesCtrl.js
@@ -1,21 +1,29 @@
 app.controller('MoviesCtrl', ['$scope', 'movies', function($scope, movies) {
   $scope.isDeleting = false;
-
   $scope.movies = movies.getMovies();
+  //$scope.movie = $scope.movies[2];
+  // $scope.movie1 = $scope.movies[0];
+  // $scope.movie2 = $scope.movies[1];
+  // $scope.movie3 = $scope.movies[2];
 
-  $scope.addMovie = function(){
+  $scope.addMovie = function() {
     alert("This functionality will be available soon!");
   };
-  
-  $scope.deleteMovies = function(){
+
+  $scope.deleteMovies = function() {
     $scope.isDeleting = true;
   };
-  
-  $scope.doneDeleteMovies = function(){
+
+  $scope.doneDeleteMovies = function() {
     $scope.isDeleting = false;
   };
-  
-  $scope.deleteMovie = function(){
+
+  $scope.deleteMovie = function(index) {
     // TODO: delete the movie only if the app is in "delete mode"
+    if ($scope.isDeleting) {
+      console.log("index is: " + index);
+      movies.removeMovie(index);
+    }
   };
- }]);
+
+}]);
